8cc32bdcdbad6f0fec6066b2fd5323beb612d13b
Kate Grant <hello.kategrant@gmail.com>
2022-11-22 22:20:10 -0500

    import Root

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import store from "./store";
 import { Provider } from "react-redux";
 import "../public/index.css";
+import Root from "./components/Root";
 
 const root = createRoot(document.getElementById("root"));
 
