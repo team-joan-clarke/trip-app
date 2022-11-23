c408ffe73591dc148a3a33b7cda7e1a3bd7625d8
Kate Grant <hello.kategrant@gmail.com>
2022-11-22 21:51:37 -0500

    added db

---
+++ db/db.js
@@ -0,0 +1,24 @@
+const { Sequelize } = require("sequelize");
+const dbUrl =
+  process.env.DATABASE_URL || `postgres://localhost:5432/yourDBName`;
+
+let config;
+if (process.env.DATABASE_URL) {
+  config = {
+    logging: false,
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  };
+} else {
+  config = {
+    logging: false,
+  };
+}
+const db = new Sequelize(dbUrl, config);
+
+module.exports = db;
