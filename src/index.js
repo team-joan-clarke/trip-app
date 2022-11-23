8023759dbfd9725376086faa0870009edd908704
Kate Grant <hello.kategrant@gmail.com>
2022-11-22 21:48:10 -0500

    imported css to frontend entrypoint

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import store from "./store";
 import { Provider } from "react-redux";
+import "../public/index.css";
 
 const root = createRoot(document.getElementById("root"));
 
