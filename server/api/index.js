920bc2990f4cc93e006789a7d9e7608c8d949c75
Kate Grant <hello.kategrant@gmail.com>
2022-11-22 21:03:00 -0500

    added main api router

---
+++ server/api/index.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+router.use("/route1", require("./route1"));
+
+router.use("/route2", require("./route2"));
+
+router.use((req, res, next) => {
+  const err = new Error("API route not found!");
+  err.status = 404;
+  next(err);
+});
+
+module.exports = router;
