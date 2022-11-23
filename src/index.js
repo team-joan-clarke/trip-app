12d2c043ee40390343ab85730503a1958692b36a
Kate Grant <hello.kategrant@gmail.com>
2022-11-22 21:46:44 -0500

    rm unused imports

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
-import axios from "axios";
 import store from "./store";
 import { Provider } from "react-redux";
-
-const Root = () => {
-  let [stuff, setStuff] = useState("app");
-  useEffect(() => {
-    async function getStuff() {
-      const someStuff = await axios.get("/Thing1");
-      setStuff(someStuff.data);
-    }
-    getStuff();
-  }, []);
-
-  return <h1>Some stuff: {JSON.stringify(stuff)}</h1>;
-};
 
 const root = createRoot(document.getElementById("root"));
 
