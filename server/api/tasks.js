e31d3e03d94d0d5ae59223d0e9b999afc0e6588d
Kate Grant <hello.kategrant@gmail.com>
2022-11-28 15:22:14 -0500

    added task GET routes by user nad by trip, includes user editors and attendees

--- server/api/tasks.js
+++ server/api/tasks.js
@@ -1,8 +1,62 @@
 const taskRouter = require("express").Router();
 const {
-  models: { Task },
+  models: { Task, User, Trip, User_Task },
 } = require("../../db");
 const Sequelize = require("sequelize");
+
+taskRouter.get("/user/:userId", async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findByPk(userId);
+    if (user) {
+      const data = await User.findAll({
+        where: { id: userId },
+        include: [{ model: Task }],
+      });
+      if (data) {
+        const tasks = data[0]["Tasks"];
+        if (tasks.length > 0) {
+          res.status(200).send(tasks);
+        } else {
+          console.log(new Error("No task data for user in Get User Tasks."));
+        }
+      } else {
+        console.log(new Error("Could not fetch task data in Get User Tasks."));
+      }
+    } else {
+      console.log(new Error("Could not fetch user data in Get User Tasks."));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+taskRouter.get("/trip/:tripId", async (req, res, next) => {
+  try {
+    const { tripId } = req.params;
+    const trip = await User.findByPk(tripId);
+    if (trip) {
+      const data = await Trip.findAll({
+        where: { id: tripId },
+        include: [{ model: Task, include: [{ model: User }] }],
+      });
+      if (data) {
+        const tasks = data[0]["Tasks"];
+        if (tasks.length > 0) {
+          res.status(200).send(tasks);
+        } else {
+          console.log(new Error("No task data for user in Get Trip Tasks."));
+        }
+      } else {
+        console.log(new Error("Could not fetch task data in Get Trip Tasks."));
+      }
+    } else {
+      console.log(new Error("Could not fetch user data in Get Trip Tasks."));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 taskRouter.post("/", async (req, res, next) => {
   try {
