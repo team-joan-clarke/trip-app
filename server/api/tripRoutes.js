3c14cb381313e71ad125e1a50ecc980350ca553e
Anahis Valenzuela <Anahisvq7@gmail.com>
2022-11-28 14:45:35 -0500

    get route to get all trips associated with a user

--- server/api/tripRoutes.js
+++ server/api/tripRoutes.js
@@ -1,11 +1,11 @@
 const tripRouter = require("express").Router();
 const {
-  models: { Trip, Task, User },
+  models: { Trip, Task, User, User_Trip },
 } = require("../../db/index");
 const Sequelize = require("sequelize");
 
-// get route to get a single Trip uses trip id to search for trip includes users and tasks
-tripRouter.get("/:tripId", async (req, res, next) => {
+// get route to get a single Trip uses trip id includes users and their role
+tripRouter.get("/singleTrip/:tripId", async (req, res, next) => {
   try {
     const findSingleTripAndUsers = await Trip.findOne({
       where: { id: req.params.tripId },
@@ -14,19 +14,23 @@ tripRouter.get("/:tripId", async (req, res, next) => {
         through: "User_Trip",
       },
     });
-
-    const findTasksForTripAndUsers = await Task.findAll({
-      where: { TripId: req.params.tripId },
-      include: {
-        model: User,
-        through: "User_Tasks",
-      },
-    });
-
-    res.send({ findSingleTripAndUsers, findTasksForTripAndUsers }).status(200);
+    res.send(findSingleTripAndUsers).status(200);
   } catch (error) {
     next(error);
   }
+});
+
+// get route to get all trips associated with a user no tasks
+tripRouter.get("/allUserTrips/:userId", async (req, res, next) => {
+  const findAllTrips = await User_Trip.findAll({
+    where: { UserId: req.params.userId },
+  });
+
+  const allTripsForUser = await Promise.all(findAllTrips.map(async (trip) => {
+    return await Trip.findByPk(trip.TripId)
+  }));
+
+  res.send(allTripsForUser);
 });
 
 // post route to create a trip
