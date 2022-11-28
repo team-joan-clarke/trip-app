95475a472c159f2aff2bbcac6da19349f8393957
Esther Yoo <liletchie@gmail.com>
2022-11-28 15:24:31 -0500

    cookie functions

--- client/redux/users.js
+++ client/redux/users.js
@@ -1,5 +1,28 @@
 import axios from "axios";
 
+// Cookie functions:
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 //action types
 const SET_USER = "SET_USER";
 
@@ -14,6 +37,7 @@ const setUser = (user) => {
 //thunks
 export const fetchUser = (id) => async (dispatch) => {
   try {
+    setCookie("user", id, 1);
     const { data: user } = await axios.get(`/api/users/${id}`);
     dispatch(setUser(user));
   } catch (error) {
