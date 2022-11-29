ea5f59a36480b2fb6c63b51d92ddd5c68c4fee89
aphantha <alisonphanthavong@gmail.com>
2022-11-29 12:25:46 -0500

    Feature: modification of user model, addition of auth folder in server directory, creation of auth folder in redux store

---
+++ server/auth/index.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+const { User } = require("../db");
+
+router.post("/login", async (req, res, next) => {
+  try {
+    res.send({ token: await User.authenticate(req.body) });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/signup", async (req, res, next) => {
+  try {
+    const { firstName, lastName, username, password, email, phoneNumber } = req.body;
+    const user = await User.create({
+      firstName,
+      lastName,
+      username,
+      password,
+      email,
+      phoneNumber
+    });
+    res.send({ token: await user.generateToken() });
+  } catch (err) {
+    if (err.name === "SequelizeUniqueConstraintError") {
+      res.status(401).send("User already exists");
+    } else {
+      next(err);
+    }
+  }
+});
+
+router.get("/verified", async (req, res, next) => {
+  try {
+    res.send(await User.findByToken(req.headers.authorization));
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+module.exports = router;
