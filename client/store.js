84dd9788869484860aa255ba675f5ebfd14ea82b
Anahis Valenzuela <Anahisvq7@gmail.com>
2022-11-29 12:18:50 -0500

    added trip reducer in store.js

--- client/store.js
+++ client/store.js
@@ -1,11 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import axios from "axios";
 import usersReducer from "./redux/users";
+import tripReducer from "./redux/tripReducer";
+
 import reducer2 from "./redux/reducer2";
 import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
 
-const reducer = combineReducers({ users: usersReducer, reducer2 });
+const reducer = combineReducers({ users: usersReducer, trips: tripReducer,reducer2 });
 
 const store = createStore(
   reducer,
