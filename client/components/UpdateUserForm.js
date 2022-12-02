9155211fd1bdcfef4ace488204bca2c05383d8d0
aphantha <alisonphanthavong@gmail.com>
2022-12-02 18:35:17 -0500

    Feature: updated singleuser and updateuserform to reflect new user state

--- client/components/UpdateUserForm.js
+++ client/components/UpdateUserForm.js
@@ -68,7 +68,7 @@ export class UpdateUser extends React.Component {
     const phoneNumber = this.props.user.phoneNumber || "";
 
     const show = this.state.show;
-
+    console.log('PROPS', this.props)
     return (
       <div
         style={{
@@ -198,7 +198,7 @@ export class UpdateUser extends React.Component {
   }
 }
 const mapState = (state) => ({
-  user: state.users,
+  user: state.users.allUsers,
   error: state.users.error,
 });
 
