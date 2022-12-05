5705538fa17461d4d0b06c49fe89a2c39d79280b
Esther Yoo <liletchie@gmail.com>
2022-12-05 15:39:24 -0500

    due date sorting of in progress tasks

--- client/components/TaskComponents/AllTasks/TasksInProgress.js
+++ client/components/TaskComponents/AllTasks/TasksInProgress.js
@@ -9,6 +9,70 @@ import TaskModal from "./TaskModal";
 import { useNavigate } from "react-router-dom";
 import { AvatarGroup, Avatar, Tooltip } from "@mui/material";
 
+//Avatar Things:
+function stringToColor(string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = "#";
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
+function stringAvatar(name) {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+  };
+}
+
+//Time Display:
+function timeDisplayConverter(time) {
+  const formattedTime = new Date(time).toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+  return formattedTime;
+}
+
+//Sorting by Due Date of Task:
+function dueDateCompare(a, b) {
+  const aTime = new Date(a.due_date);
+  const bTime = new Date(b.due_date);
+  if (aTime) {
+    if (bTime) {
+      if (aTime >= bTime) {
+        return 1;
+      } else {
+        return -1;
+      }
+    } else {
+      return 1;
+    }
+  } else {
+    if (bTime) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+}
+
+// Actual Component:
 const TasksInProgress = (props) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -26,37 +90,9 @@ const TasksInProgress = (props) => {
   const [show, setShow] = useState(false);
 
   const tasks = props.tasks.allItineraryTasks || [];
-  let inProgressTasks = tasks.filter((task) => task.status === "in progress");
-
-  //Avatar Things:
-  function stringToColor(string) {
-    let hash = 0;
-    let i;
-
-    /* eslint-disable no-bitwise */
-    for (i = 0; i < string.length; i += 1) {
-      hash = string.charCodeAt(i) + ((hash << 5) - hash);
-    }
-
-    let color = "#";
-
-    for (i = 0; i < 3; i += 1) {
-      const value = (hash >> (i * 8)) & 0xff;
-      color += `00${value.toString(16)}`.slice(-2);
-    }
-    /* eslint-enable no-bitwise */
-
-    return color;
-  }
-
-  function stringAvatar(name) {
-    return {
-      sx: {
-        bgcolor: stringToColor(name),
-      },
-      children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
-    };
-  }
+  let inProgressTasks = tasks
+    .filter((task) => task.status === "in progress")
+    .sort(dueDateCompare);
 
   return (
     <div>
@@ -111,7 +147,10 @@ const TasksInProgress = (props) => {
 
                   <Card.Title>{singleTask.type} </Card.Title>
                   <Card.Text>Trip Name: {singleTask.Trip.name}</Card.Text>
-                  <Card.Text>Task Due Date: {singleTask.due_date}</Card.Text>
+                  <Card.Text>
+                    Task Due Date:{" "}
+                    {new Date(singleTask.due_date).toLocaleDateString()}
+                  </Card.Text>
                   <Card.Text>
                     Provider Name: {singleTask.provider_name}
                   </Card.Text>
@@ -119,13 +158,42 @@ const TasksInProgress = (props) => {
                     Booking Number: {singleTask.booking_num}
                   </Card.Text>
                   <Row>
-                    <Col>
-                      <Card.Text>Start Date: {singleTask.start_date}</Card.Text>
-                    </Col>
-                    <Col>
-                      <Card.Text>End Date: {singleTask.end_date}</Card.Text>
-                    </Col>
+                    {singleTask.start_date && (
+                      <div>
+                        <Col>
+                          <Card.Text>
+                            Start Date:
+                            {new Date(
+                              singleTask.start_date
+                            ).toLocaleDateString()}
+                          </Card.Text>
+                        </Col>
+                        <Col>
+                          <Card.Text>
+                            Start Time:
+                            {timeDisplayConverter(singleTask.start_date)}
+                          </Card.Text>
+                        </Col>
+                      </div>
+                    )}
+                    {singleTask.end_date && (
+                      <div>
+                        <Col>
+                          <Card.Text>
+                            End Date:
+                            {new Date(singleTask.end_date).toLocaleDateString()}
+                          </Card.Text>
+                        </Col>
+                        <Col>
+                          <Card.Text>
+                            End Time:
+                            {timeDisplayConverter(singleTask.end_date)}
+                          </Card.Text>
+                        </Col>
+                      </div>
+                    )}
                   </Row>
+
                   <Card.Text>Link: {singleTask.link}</Card.Text>
                   <Card.Text>Description: {singleTask.description}</Card.Text>
 
