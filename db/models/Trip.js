2969e4e3aa420b9fa3526a25430f889659a43316
aphantha <alisonphanthavong@gmail.com>
2022-12-05 20:00:03 -0500

    Feature: create trip validation

--- db/models/Trip.js
+++ db/models/Trip.js
@@ -1,40 +1,54 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const db = require("../db");
 
-const Trip = db.define("Trip", {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  city: {
-    type: DataTypes.STRING,
-    defaultValue: "TBD",
-  },
-  state: {
-    type: DataTypes.STRING,
-    defaultValue: "TBD",
-  },
-  country: {
-    type: DataTypes.STRING,
-    defaultValue: "TBD",
-  },
-  start_date: {
-    type: DataTypes.DATE,
-  },
-  end_date: {
-    type: DataTypes.DATE,
-  },
-  status: {
-    type: DataTypes.ENUM("active", "complete"),
-  },
-  imageUrl: {
-    type: DataTypes.TEXT,
-    validate: {
-      isUrl: true,
+const Trip = db.define(
+  "Trip",
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    defaultValue:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPalFmzItiv41uwG0LGteZ-243tFftPPUb1xfU8MQNo-iEOpBBT_Kflw56iuun22IgT-M&usqp=CAU",
+    city: {
+      type: DataTypes.STRING,
+      defaultValue: "TBD",
+    },
+    state: {
+      type: DataTypes.STRING,
+      defaultValue: "TBD",
+    },
+    country: {
+      type: DataTypes.STRING,
+      defaultValue: "TBD",
+    },
+    start_date: {
+      type: DataTypes.DATE,
+    },
+    end_date: {
+      type: DataTypes.DATE,
+    },
+    status: {
+      type: DataTypes.ENUM("active", "complete"),
+    },
+    imageUrl: {
+      type: DataTypes.TEXT,
+      validate: {
+        isUrl: true,
+      },
+      defaultValue:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPalFmzItiv41uwG0LGteZ-243tFftPPUb1xfU8MQNo-iEOpBBT_Kflw56iuun22IgT-M&usqp=CAU",
+    },
   },
-});
+  {
+    validate: {
+      startDateAfterEndDate() {
+        if (this.end_date) {
+          if (this.start_date > this.end_date) {
+            throw new Error("Start date must be before the end date.");
+          }
+        }
+      },
+    },
+  }
+);
 
 module.exports = Trip;
