0faf96c287d2166d3005747b4f2ba2d434bc4439
Anahis Valenzuela <anahisvq7@gmail.com>
2024-02-21 15:09:45 -0800

    changed html code on mail.js and confirmationEmail.js

--- server/api/confirmationEmail.js
+++ server/api/confirmationEmail.js
@@ -3,6 +3,36 @@ const nodemailer = require("nodemailer");
 const { requireToken } = require("./gatekeepingmiddleware");
 
 mailRouter2.post("/sendEmailToPersonWhoReferred", requireToken, async (req, res) => {
+  const htmlToSend = `
+  <!DOCTYPE html>
+  <html lang="en>
+  <head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <meta content="telephone=no" name="format-detection">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <link rel="stylesheet" href="../../index.css"/>
+    <title>Confirmation email that the user you invited joined trippn</title>
+  </head>
+  <body>
+    <table align="center">
+      <tbody>
+        <tr>
+          <td align="center" style="background-color: #fcfaf4;">
+          <img style="margin-top: 30px" alt="trippn's circular palm tree logo" src="https://github.com/team-joan-clarke/trip-app/blob/main/public/logo.png?raw=true"/>
+          <h1>trippn</h1>
+          <h3>Hello there trip planner 👋 </h3>
+          <p>The trip planner you invited: ${req.body.firstName} has joined trippn! </p> 
+          <h4>Click the button below to continue planning your trip 🗺️ </h4>
+          <a href="https://trippn.onrender.com/login" style="display: inline-block;  padding: 10px 20px; width: 300px; background-color: lightseagreen; background-image: linear-gradient(115deg, lightseagreen, 68%, #bded7e);; color: #fff; text-align: center; text-decoration: none; border-radius: 10px; font-family: Arial, sans-serif;">Continue Planning</a>
+          <p>trippn team ✈️ </p>
+           </td>
+        </tr>
+      </tbody>
+    </table>
+  </body>
+  </html>`
+  
 
   var transporter = nodemailer.createTransport({
     port: 465,
@@ -11,13 +41,13 @@ mailRouter2.post("/sendEmailToPersonWhoReferred", requireToken, async (req, res)
       user: "trippnwebsite@gmail.com",
       pass: process.env.APP_PASS,
     },
-  });
+  })
 
   var mailOptions = {
     from: "trippnwebsite@gmail.com",
     to: `${req.body.doTheyHaveReferralEmail}`,
     subject: `The person you invited: ${req.body.firstName} joined trippn!`,
-    text: `Hello there, the person you invited has joined trippn. ✈️ Navigate to the link to continue planning your trip! https://trippn.onrender.com/login thanks, trippn team`,
+    html: htmlToSend,
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
--- server/api/mail.js
+++ server/api/mail.js
@@ -1,28 +1,39 @@
 const mailRouter = require("express").Router();
 const nodemailer = require("nodemailer");
-const {
-    requireToken,
-  } = require("./gatekeepingmiddleware");
+const { requireToken } = require("./gatekeepingmiddleware");
 
-  
-  mailRouter.post("/text-mail", requireToken, (req, res) => {
-    console.log("in mail post route")
-
+mailRouter.post("/text-mail", requireToken, (req, res) => {
   const htmlToSend = `
   <!DOCTYPE html>
-  <html>
+  <html lang="en>
   <head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <meta content="telephone=no" name="format-detection">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <link rel="stylesheet" href="../../public/index.css"/>
     <title>Invitation to trippn</title>
   </head>
+
   <body>
-    <div>
-      <p>Hello there, </p>
-      <p>${req.body.referralEmail} has invited you to join trippn! </p> 
-      <p>Upon sign up add the email of the person who invited you to the referral email input field.</p>
-      <p>Navigate to the link to get started https://trippn.onrender.com/signup</p>
-      <p>Thanks,</p>
-      <p>trippn team ✈️ </p>
-    </div>
+    <table align="center">
+      <tbody>
+        <tr>
+          <td align="center" style="background-color: #fcfaf4;">
+          <img style="margin-top: 30px" alt="trippn's circular palm tree logo" src="https://github.com/team-joan-clarke/trip-app/blob/main/public/logo.png?raw=true"/>
+          <h1>trippn</h1>
+          <h3>Hello there trip planner 🏞 </h3>
+          <p>Your fellow trip planner ${req.body.referralEmail} has invited you to join trippn! </p> 
+          <h4>How it works ✍️</h4>
+          <p style="margin: 20px">When completing the sign up form add the email of the person who invited you to the referral email input field.</p>
+          <h4>Click the button below to get started planning your trip 🗺️ </h4>
+          <a href="https://trippn.onrender.com/signup"  style="display: inline-block; padding: 10px 20px; width: 300px; background-color: lightseagreen; background-image: linear-gradient(115deg, lightseagreen, 68%, #bded7e); color: #fff; text-align: center; text-decoration: none; border-radius: 10px; font-family: Arial, sans-serif;">Accept Invite</a>
+          <h4>Bon voyage & safe travels 👋 </h4>
+          <p>trippn team ✈️ </p>
+           </td>
+        </tr>
+      </tbody>
+    </table>
   </body>
   </html>`;
 
@@ -40,7 +51,7 @@ const {
     to: `${req.body.recipient}`,
     subject: "You have been invited to join trippn",
     text: "",
-    html: htmlToSend
+    html: htmlToSend,
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
@@ -52,8 +63,4 @@ const {
   });
 });
 
-
-
 module.exports = mailRouter;
-
-
